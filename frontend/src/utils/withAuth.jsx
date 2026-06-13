2dd50e86ef960227705b926fc5c896240416c888
Utkarsh Roy <utkarshroy186@gmail.com>
2026-06-13 14:16:58 +0530

    Update withAuth.jsx

--- frontend/src/utils/withAuth.jsx
+++ frontend/src/utils/withAuth.jsx
@@ -1,27 +1,24 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-const withAuth = (WrappedComponent ) => {
+const withAuth = (WrappedComponent) => {
     const AuthComponent = (props) => {
         const router = useNavigate();
 
         const isAuthenticated = () => {
-            if(localStorage.getItem("token")) {
-                return true;
-            } 
-            return false;
-        }
+            return !!localStorage.getItem("token");
+        };
 
         useEffect(() => {
-            if(!isAuthenticated()) {
-                router("/auth")
+            if (!isAuthenticated()) {
+                router("/auth");
             }
-        }, [])
+        }, [router]);
 
-        return <WrappedComponent {...props} />
-    }
+        return <WrappedComponent {...props} />;
+    };
 
     return AuthComponent;
-}
+};
 
 export default withAuth;
